9daa67026396a2625a53ed5418b077eee235334c
ABHILASH <ABHILASH.SHARMA47@YAHOO.IN>
2020-10-20 22:39:29 +0530

    added order summary and button component

--- src/components/Burger/OrderSummary/OrderSummary.jsx
+++ src/components/Burger/OrderSummary/OrderSummary.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Aux from '../../../hoc/Auxiliary';
+import Button from '../../UI/Button/Button';
 
 const orderSummary = ( props ) => {
     const ingredientsSummary = Object.keys(props.ingredients)
@@ -24,11 +25,19 @@ const orderSummary = ( props ) => {
                 { ingredientsSummary }
             </ul>
             <p>
-                Price: ${ props.price }
+                Total Price: ${ props.price.toFixed(2) }
             </p>
             <p>
                 Proceed to checkout?
             </p>
+            <Button btnType="Danger"
+                clicked={ props.purchaseCancelled }>
+                CANCEL
+            </Button>
+            <Button btnType="Success"
+                clicked={ props.purchaseContinued }>
+                CONTINUE
+            </Button>
         </Aux>
     );
 }
--- src/containers/BurgerBuilder/BurgerBuilder.jsx
+++ src/containers/BurgerBuilder/BurgerBuilder.jsx
@@ -93,6 +93,10 @@ class BurgerBuilder extends Component {
         });
     }
 
+    purchaseContinueHandler = () => {
+        alert('You continued');
+    }
+
     render() {
         const disabledInfo = {
             ...this.state.ingredients
@@ -107,7 +111,9 @@ class BurgerBuilder extends Component {
                 <Modal show={ this.state.purchasing }
                     modalClosed={ this.purchaseCancelHandler }>
                     <OrderSummary ingredients={ this.state.ingredients }
-                        price={ this.state.totalPrice }></OrderSummary>
+                        price={ this.state.totalPrice }
+                        purchaseCancelled={ this.purchaseCancelHandler }
+                        purchaseContinued={ this.purchaseContinueHandler } />
                 </Modal>
                 <Burger ingredients={ this.state.ingredients } />
                 <BuildControls 
