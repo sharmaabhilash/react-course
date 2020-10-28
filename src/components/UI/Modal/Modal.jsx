cd7708aed6e0430fa880aa057a39e9791d7df2e0
ABHILASH <ABHILASH.SHARMA47@YAHOO.IN>
2020-10-28 22:35:51 +0530

    add menu for mobile devices adjust settings for mobile devices

--- src/components/UI/Modal/Modal.jsx
+++ src/components/UI/Modal/Modal.jsx
@@ -1,21 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './Modal.css';
 import Aux from '../../../hoc/Auxiliary';
 import Backdrop from '../Backdrop/Backdrop';
 
-const modal = ( props ) => {
-    return (
-        <Aux>
-            <Backdrop show={ props.show } clicked={ props.modalClosed }></Backdrop>
-            <div className={ classes.Modal }
-                style={{
-                    transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-                    opacity: props.show ? '1' : '0'
-                }}>
-                { props.children }
-            </div>
-        </Aux>
-    );
+class Modal extends Component {
+
+    /** 
+     * This will check if show state is changed or not, if changed then only the 'modal' component is called or updated, else it will not get called. Also component wrapped inside 'modal' component is also not called.
+     * */
+    shouldComponentUpdate (nextProps, nextState) {
+        return nextProps.show != this.props.show;
+    }
+
+    componentDidUpdate () {
+        console.log('[Modal] component');
+    }
+
+    render () {
+        return (
+            <Aux>
+                <Backdrop show={ this.props.show } clicked={ this.props.modalClosed }></Backdrop>
+                <div className={ classes.Modal }
+                    style={{
+                        transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                        opacity: this.props.show ? '1' : '0'
+                    }}>
+                    { this.props.children }
+                </div>
+            </Aux>
+        );
+    }
 }
 
-export default modal;
+export default Modal;
