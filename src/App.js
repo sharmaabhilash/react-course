222c0d8f3a679169b3fa48ebe51a9ea4e92bd0cc
ABHILASH <ABHILASH.SHARMA47@YAHOO.IN>
2020-11-05 23:11:13 +0530

    routing project setup

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React, { Component } from 'react';
-import './App.css';
-import Layout from './hoc/Layout/Layout';
-import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+
+import Blog from './containers/Blog/Blog';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Layout>
-          <BurgerBuilder></BurgerBuilder>
-        </Layout>
+        <Blog />
       </div>
     );
   }
