ccc5b8ca164cb66626365966666e38aa1f359958
Abhilash Sharma <Abhilash.Sharma@Advaiya.com>
2020-11-17 21:24:49 +0530

    lazy loading

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -3,7 +3,12 @@ import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
-import NewPost from './NewPost/NewPost';
+import asyncComponent from '../../hoc/asyncComponent';
+// import NewPost from './NewPost/NewPost';
+
+const AsyncNewPost = asyncComponent(() => {
+    return import('./NewPost/NewPost');
+});
 
 class Blog extends Component {
     render () {
@@ -27,7 +32,7 @@ class Blog extends Component {
                 </header>
                 {/* <Route path="/" exact render={ () => <h1>Home</h1> } /> */}
                 <Switch>
-                    <Route path="/new-post" component={ NewPost } />
+                    <Route path="/new-post" component={ AsyncNewPost } />
                     <Route path="/posts/" component={ Posts } />
                     {/* <Route render={ () => <h1>Not Found</h1> } /> */}
                     <Redirect from="/" to="/posts/" />
